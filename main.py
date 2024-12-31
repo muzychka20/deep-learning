9720362c975dd131ea9475e3652bb2adf2d71b5d
Kyrylo Muzychka <kyryll2016@gmail.com>
2024-12-31 10:51:52 +0200

    created model

---
+++ main.py
@@ -0,0 +1,54 @@
+import numpy as np
+from random import randint
+from sklearn.utils import shuffle
+from sklearn.preprocessing import MinMaxScaler
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+
+train_labels = []
+train_samples = []
+
+for i in range(50):
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(1)
+    
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(0)
+    
+for i in range(1000):
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(0)
+    
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(1)
+    
+# for sample, label in zip(train_samples, train_labels):
+#     print(sample, label)
+
+train_labels = np.array(train_labels)
+train_samples = np.array(train_samples)
+train_labels, train_samples = shuffle(train_labels, train_samples)
+
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1, 1))
+
+model = Sequential([
+    Dense(units=16, input_shape=(1,), activation='relu'),
+    Dense(units=32, activation='relu'),
+    Dense(units=2, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=30, shuffle=True, verbose=2)
