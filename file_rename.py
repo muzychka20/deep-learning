b98516b361493d33f04d0d23fe68705c41ac071b
Kyrylo Muzychka <kyryll2016@gmail.com>
2025-01-02 09:14:50 +0200

    update photos

--- file_rename.py
+++ file_rename.py
@@ -1,14 +1,14 @@
 import os
 
-def rename_files_with_numbers(directory):    
+def rename_files_with_numbers(directory, prefix_name):    
     files = os.listdir(directory)
     files.sort()
 
     for index, filename in enumerate(files, start=1):    
         file_name, file_extension = os.path.splitext(filename)
-        # file_name = file_name[8:]
-        # Формируем новое имя, добавляя число в начало
-        new_name = f"dog_{file_name}{file_extension}"
+        
+        # Формируем новое имя, добавляя число
+        new_name = f"{prefix_name}_{file_name}{file_extension}"
         
         # Получаем полный путь для старого и нового имени файла
         old_path = os.path.join(directory, filename)
@@ -19,4 +19,5 @@ def rename_files_with_numbers(directory):
         print(f"Файл переименован: {filename} -> {new_name}")
 
 
-rename_files_with_numbers('./dogs-vs-cats/1')
+rename_files_with_numbers('./pet-images/Cat', 'cat')
+rename_files_with_numbers('./pet-images/Dog', 'dog')
